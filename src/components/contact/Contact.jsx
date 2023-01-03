0e028af58019704e9930eaf503fb62031521ff05
sinagss <sina.ghassai@gmail.com>
2023-01-03 21:22:39 +0330

    finished project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import { MdEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
 import { FaTelegramPlane } from "react-icons/fa";
 
+// import { emailjs } from "emailjs-com";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qp4xiaf",
+        "template_azcprd3",
+        form.current,
+        "02H5cSnnGKJUh1OXB"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -14,7 +41,7 @@ const Contact = () => {
         <div className="contact__options">
           {/* Email */}
           <article className="contact__option">
-            <MdEmail />
+            <MdEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>maryam.tahbaz65@gmail.com</h5>
             <a
@@ -28,7 +55,7 @@ const Contact = () => {
 
           {/* Telegram */}
           <article className="contact__option">
-            <FaTelegramPlane />
+            <FaTelegramPlane className="contact__option-icon" />
             <h4>Telegram</h4>
             <h5>@maryamTbz</h5>
             <a href="https://t.me/maryamTbz" target={"_blank"} rel="noreferrer">
@@ -38,7 +65,7 @@ const Contact = () => {
 
           {/* WhatsApp */}
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+989366730866</h5>
             <a
@@ -53,43 +80,48 @@ const Contact = () => {
         {/* END OF CONTACT OPTIONS */}
 
         {/* FORM */}
-        <form action="" method="post">
+        <form ref={form} onSubmit={sendEmail}>
           {/* NAME INPUT */}
+          <label htmlFor="name">Full Name:</label>
           <input
             type="text"
             name="name"
-            id=""
+            id="name"
             placeholder="Your Full Name"
             aria-label="name"
             required
           />
 
           {/* COMPANY NAME */}
+          <label htmlFor="companyName">Company Name:</label>
           <input
             type="text"
             name="companyName"
-            id=""
-            placeholder="Your Full Name"
+            id="companyName"
+            placeholder="Your company name"
             aria-label="company name"
           />
 
           {/* EMAIL INPUT */}
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             name="email"
-            id=""
+            id="email"
             placeholder="Your Email"
             aria-label="email"
             required
           />
 
           {/* MESSAGE INPUT */}
+          <label htmlFor="message">Message:</label>
           <textarea
             name="message"
-            id=""
+            id="message"
             cols="30"
             rows="10"
             placeholder="Your Message"
+            aria-label="Message"
           ></textarea>
 
           {/* SUBMIT BUTTON */}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#home" className="footer__logo">
+        MARYAM TAHBAZ
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com">
+          <FaGithubSquare />
+        </a>
+        <a href="https://www.linkedin.com/in/maryam-tahbaz-283868119?originalSubdomain=ir">
+          <BsLinkedin />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Maryam Tahbaz. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
