2cc057325bf7f7a9f9d8fb1b06d33aa9ab6af4e0
sinagss <sina.ghassai@gmail.com>
2023-01-10 22:56:26 +0330

    changed download cv button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/cv.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={CV} download className="button2">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
